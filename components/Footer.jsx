c3a244dd7fefec8b1e62625dfa0bbb7c2ed1e52f
ericz02 <ezheng0720@gmail.com>
2023-05-15 10:16:04 -0400

    adds footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,9 @@
 const Footer = () => {
-
     return (
-        <div>
-
-        </div>
+        <footer className="bg-gray-200 py-4 text-center">
+        <p className="text-gray-600">© 2023 My Website. All rights reserved.</p>
+        </footer>
     )
 }
 
-export default Footer
+export default Footer;
