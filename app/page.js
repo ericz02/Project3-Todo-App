d787b35f69b95870ea33d07e601ded3212835244
ericz02 <ezheng0720@gmail.com>
2023-05-15 10:06:43 -0400

    adds client side component for routing

--- app/page.js
+++ app/page.js
@@ -1,6 +1,18 @@
+// useRouter hook to a client components
+'use client'
+
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const handleClick = () => {
+      router.push('/login')
+      
+  }
+  
   return (
     <main>
       <div className="mt-60px">
@@ -14,7 +26,7 @@ export default function Home() {
         
         </h1>
 
-        <button className="absolute top-3/4 left-1/2 transform -translate-x-1/2 
+        <button onClick={handleClick} className="absolute top-3/4 left-1/2 transform -translate-x-1/2 
         -translate-y-1/ z-10 bg-text-white bg-gradient-to-r from-cyan-500 to-blue-500
          hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800
         font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Get Started</button>
