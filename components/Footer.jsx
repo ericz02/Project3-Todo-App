300691478621efc93757b07f36e29fa9c3141faa
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2023-05-22 19:05:59 -0400

    user change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,30 @@
 "use client"
-import useUser from '../hooks/useUser'
+import { useRouter } from "next/navigation";
+import useUser from '../hooks/useUser';
 
 const Footer = () => {
-    const { user, loading } = useUser(); 
-
+    const { user, loading } = useUser();
+    const router = useRouter();
+    
     if (loading) return <p>Loading...</p>;
 
     return (
-      <div>
-        {user ? 'User' : 'Login & Register'}
-      </div>
+        <div className="flex justify-between mt-5">
+            {
+                user ? (
+                    <>
+                        <a onClick={() => router.replace('/profile')}>Profile</a>
+                        <a onClick={() => router.replace('/logout')}>Logout</a>
+                    </>
+                ) : ( 
+                    // User is not logged in
+                    <>
+                        <a onClick={() => router.replace('/login')}>Login</a>
+                        <a onClick={() => router.replace('/register')}>Register</a>
+                    </>
+                )
+            }
+        </div>
     )
 }
 
