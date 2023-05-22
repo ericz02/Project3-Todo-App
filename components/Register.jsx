3d50614000f841cc967a94ad17f4527a0157a3eb
Jerrry-10 <jerryaviles10@gmail.com>
2023-05-22 10:45:53 -0400

    register

--- components/Register.jsx
+++ components/Register.jsx
@@ -1,71 +1,119 @@
-"use client"
+"use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useReducer } from "react";
+import { registerUser } from "../utils/data.js";
 
 const Register = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const router = useRouter();
 
-    const handleSubmit = (e) => {
+  function reducer(state, action) {
+    switch (action.type) {
+      case "email":
+      case "name":
+      case "password":
+      case "slug":
+        return { ...state, [action.type]: action.value };
+      case "loading":
+        return { ...state, loading: action.loading };
+      case "response":
+        return { ...state, response: action.response };
+    }
+
+    throw Error("Unknown action." + action.type);
+  }
+
+  const initialState = {
+    email: "",
+    name: "",
+    password: "",
+    slug: "",
+    response: "",
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { email, name, slug, password, response, loading } = state;
+
+  const register = async (e) => {
+    dispatch({ type: "loading", loading: true });
     e.preventDefault();
 
-    // Reset form fields
-    setName('');
-    setEmail('');
-    setPassword('');
-    };
+    const response = await registerUser(name, email, password, name);
+    dispatch({ type: "response", response });
+    dispatch({ type: "loading", loading: false });
+    if (response?.success) {
+      setTimeout(() => {
+        router.push("/login");
+      }, 3000);
+    }
+  };
 
-    return (
-        <div className="flex justify-center items-center h-[610px]">
-            <form className="w-1/3 p-6 bg-white rounded shadow">
-            <h2 className="text-2xl font-bold mb-6">Create Account</h2>
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-                    Name
-                </label>
-                <input
-                    className="w-full px-3 py-2 border border-gray-400 rounded"
-                    type="text"
-                    id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-            </div>
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                    Email
-                </label>
-                <input
-                    className="w-full px-3 py-2 border border-gray-400 rounded"
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </div>
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" 
-                    htmlFor="password">
-                    Password
-                </label>
-                <input
-                    className="w-full px-3 py-2 border border-gray-400 rounded"
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </div>
-            <button
-                className="w-full bg-[#5B8AC7] hover:bg-[#37639D] text-white font-bold py-2 px-4 rounded mt-5"
-                type="submit"
-                onClick={handleSubmit}>
-                Create Account
-            </button>
-            </form>
+  return (
+    <div className="flex justify-center items-center h-[610px]">
+      <form className="w-1/3 p-6 bg-white rounded shadow" onSubmit={register}>
+        <h2 className="text-2xl font-bold mb-6">Create Account</h2>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Name
+          </label>
+          <input
+            className="w-full px-3 py-2 border border-gray-400 rounded"
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => {
+              dispatch({ type: "name", value: e.target.value });
+            }}
+          />
         </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="w-full px-3 py-2 border border-gray-400 rounded"
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => {
+              dispatch({ type: "email", value: e.target.value });
+            }}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="w-full px-3 py-2 border border-gray-400 rounded"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => {
+              dispatch({ type: "password", value: e.target.value });
+            }}
+          />
+        </div>
+        <button
+          className="w-full bg-[#5B8AC7] hover:bg-[#37639D] text-white font-bold py-2 px-4 rounded mt-5"
+          type="submit"
+        >
+          Create Account
+        </button>
+      </form>
+    </div>
   );
 };
 
-export default Register
+export default Register;
---
+++ utils/data.js
@@ -0,0 +1,68 @@
+import supabase from "./supabase";
+// register a user//
+/**
+ * Register a user by passing in an email, password, name and slug
+ * @param {*} email
+ * @param {*} password
+ * @param {*} name
+ * @param {*} slug
+ * @returns plain old javascript object with success, message and optionally, the rest of the addMetaResponse.data object
+ */
+const registerUser = async (email, password, name, slug) => {
+  const { data: registerData, error: registerError } = await supabase
+    .from("profile")
+    .select("*")
+    .eq("slug", slug);
+  if (registerError) {
+    return {
+      success: false,
+      error: registerError,
+    };
+  }
+  if (registerData.length > 0) {
+    return {
+      success: false,
+      error: registerError,
+    };
+  }
+
+  const authResponse = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (authResponse.error) {
+    return {
+      success: false,
+      error: authResponse.error,
+    };
+  }
+
+  if (authResponse.data.user) {
+    const addMetaResponse = await supabase
+      .from("profile")
+      .insert([{ user_id: authResponse.data.user.id, name, slug }]);
+
+    if (addMetaResponse.error) {
+      return {
+        success: false,
+        error: addMetaResponse.error,
+      };
+    }
+    return {
+      success: true,
+      message:
+        "Registration successful, please wait a few moments to be taken to the login page",
+      ...addMetaResponse.data,
+    };
+  }
+
+  return {
+    success: false,
+    error: {
+      message: "An unknown error has occurred",
+    },
+  };
+};
+
+export { registerUser };
